3bee2b6136910e52d7201f096525a1b011b662a8
Jonas Barsten <jonasbarsten@gmail.com>
2021-01-30 15:44:47 +0100

    Re-adding react-draggable as doing it manually was too much pain and I had a good nights sleep :)

--- src/components/Fader.js
+++ src/components/Fader.js
@@ -1,76 +1,34 @@
 import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 import { scale } from "../utils";
 
 const Fader = (props) => {
   const [gain, setGain] = useState(1);
-  const [faderCanDrag, setFaderCanDrag] = useState(false);
-  const [dragState, setDragState] = useState({
-    x: null,
-    y: null,
-    px: 28.7, // Gain: 1
-    prop: null,
-  });
-  const [faderStyle, setFaderStyle] = useState({ top: `${dragState.px}px` });
+  const [position, setPosition] = useState(-171);
 
-  const handleDrag = (e, max) => {
-    if (!faderCanDrag) {
-      return;
-    }
-    const touch = e.type === "touchmove" && e.originalEvent.changedTouches;
-    const pos = touch && touch[0] ? touch[0].pageY : e.pageY;
-    const state = dragState.y;
-    const delta = pos - state;
-    let css = dragState.px + delta;
-    css = Math.min(max, css);
-    css = Math.max(0, css);
-    setFaderStyle({ top: `${css}px` });
-    setGain(Math.pow(scale(css, 0, 220, 1.15, 0), 2));
-  };
-
-  const enableDrag = (e) => {
-    let newDragState = {};
-    const elem = e.target;
-    setFaderCanDrag(true);
-    newDragState.px = parseInt(elem.style.top, 10);
-    if (e.type === "touchstart" && e.originalEvent.changedTouches) {
-      const touch = e.originalEvent.changedTouches[0];
-      newDragState.x = touch.pageX;
-      newDragState.y = touch.pageY;
-    } else {
-      newDragState.x = e.pageX;
-      newDragState.y = e.pageY;
-    }
-    setDragState(newDragState);
-  };
-
-  const disableDrag = () => {
-    if (faderCanDrag) {
-      setFaderCanDrag(false);
-    }
+  const handleDrag = (e, data, max) => {
+    setPosition(data.y);
+    const scaled = data.y + 200;
+    setGain(Math.pow(scale(scaled, 0, max, 1.15, 0), 2));
   };
 
   const resetFader = () => {
-    const top = scale(1, 0, 1.15, 220, 0);
     setGain(1);
-    setFaderStyle({ top: `${top}px` });
+    setPosition(-171);
   };
 
-  const max = props.master ? 315 : 220;
+  const max = props.master ? 314 : 220;
 
   return (
-    <div
-      className="fader"
-      style={faderStyle}
-      onMouseDown={enableDrag}
-      onTouchStart={enableDrag}
-      onDoubleClick={resetFader}
-      onMouseUp={disableDrag}
-      onTouchEnd={disableDrag}
-      onTouchCancel={disableDrag}
-      onMouseOut={disableDrag}
-      onMouseMove={(e) => handleDrag(e, max)}
-    ></div>
+    <Draggable
+      axis="y"
+      bounds="parent"
+      position={{ x: 0, y: position }}
+      onDrag={(e, data) => handleDrag(e, data, max)}
+    >
+      <div className="fader" onDoubleClick={resetFader}></div>
+    </Draggable>
   );
 };
 
